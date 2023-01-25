17b21b90850ed40bdbef9d894476b6f50c3816d1
Suneel Varma <49975815+Suneelvarma5fi@users.noreply.github.com>
2023-01-25 19:49:06 +0530

    Update detect_ranges_with_count.py

--- detect_ranges_with_count.py
+++ detect_ranges_with_count.py
@@ -1,19 +1,20 @@
-def detect_ranges(samples):
-    samples.sort()
-    current_range = [samples[0], samples[0]]
-    result = get_ranges(samples,current_range)
-    return result
-
-def get_ranges(samples,current_range):
-	count,result = 1,[]
-	for i in range(1, len(samples)):
-	    if samples[i] == current_range[1] or samples[i] == current_range[1] + 1:
-	        current_range[1] = samples[i]
-	        count += 1
-	    else:
-	        result.append((current_range[0],current_range[1],count))
-	        current_range = [samples[i], samples[i]]
-	        count = 1
-	        
-	result.append((current_range[0],current_range[1],count))
-	return result
+    
+    def generate_result(result,item):
+    	result.append(item)
+    	return result
+    
+    
+    def detect_ranges(samples):
+        samples.sort()
+        current_range,count,result = [samples[0], samples[0]],1,[]
+        for i in range(1, len(samples)):
+            if samples[i] == current_range[1] or samples[i] == current_range[1] + 1:
+                current_range[1] = samples[i]
+                count += 1
+            else:
+            	result = generate_result(result,(current_range[0],current_range[1],count))
+            	current_range = [samples[i], samples[i]]
+            	count = 1
+        result = generate_result(result,(current_range[0],current_range[1],count))
+        return result
+#print(detect_ranges([3,3,4,5,4]))
